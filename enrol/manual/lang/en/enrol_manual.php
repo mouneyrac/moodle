c978f59a93358b94404caa94d552e8829e8f0b22
Jerome Mouneyrac <jerome@moodle.com>
2011-04-28 16:41:27 +0800

    MDL-26250 Create a web service function that enrols users to a certain course

--- enrol/manual/lang/en/enrol_manual.php
+++ enrol/manual/lang/en/enrol_manual.php
@@ -53,3 +53,6 @@ $string['unenrolselectedusers'] = 'Unenrol selected users';
 $string['unenrolselfconfirm'] = 'Do you really want to unenrol yourself from course "{$a}"?';
 $string['unenroluser'] = 'Do you really want to unenrol "{$a->user}" from course "{$a->course}"?';
 $string['unenrolusers'] = 'Unenrol users';
+$string['wscannotenrol'] = 'Plugin instance cannot manually enrol a user in the course id = {$a->courseid}';
+$string['wsroledoesntexist'] = 'The role (id = {$a->roleid}) doesn\'t exist. You cannot enrol the user (id = {$a->userid}) in the course(id = {$a->courseid})';
+$string['wsusercannotassign'] = 'You don\'t have the permission to assign this role ({$a->roleid}) to this user ({$a->userid}) in this course({$a->courseid}).';

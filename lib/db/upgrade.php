7d86c88562120de4385e2fd3a6b22e60a0470176
Jerome Mouneyrac <jerome@moodle.com>
2012-10-11 14:59:04 +0800

    MDL-34426 port of the Oauth2 authentication plugin

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -1237,6 +1237,7 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2012092600.00);
     }
 
+
     if ($oldversion < 2012101500.01) {
         // Find all orphaned blog associations that might exist.
         $sql = "SELECT ba.id
@@ -1564,6 +1565,30 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2012120300.07);
     }
 
+    if ($oldversion < 2013011800.01) {
+
+        // Define table user_idps to be created.
+        $table = new xmldb_table('user_idps');
+
+        // Adding fields to table user_idps.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('component', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('provideruserid', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('email', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+
+        // Adding keys to table user_idps.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, array('userid'), 'user', array('id'));
+
+        // Conditionally launch create table for user_idps.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Main savepoint reached
+        upgrade_main_savepoint(true, 2013011800.01);
+    }
 
     return true;
 }

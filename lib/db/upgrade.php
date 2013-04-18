677937d77440f1fd9d7bb3aa4dd3ecce1353a69d
Jerome Mouneyrac <jerome@mouneyrac.com>
2013-04-18 11:24:28 +0800

    MDL-34426 port of the Oauth2 authentication plugin

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2450,5 +2450,30 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2013091300.01);
     }
 
+    if ($oldversion < 2013092000.02) {
+
+        // Define table user_idps to be created.
+        $table = new xmldb_table('user_idps');
+
+        // Adding fields to table user_idps.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('component', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('provideruserid', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('email', XMLDB_TYPE_CHAR, '100', null, null, null, null);
+
+        // Adding keys to table user_idps.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, array('userid'), 'user', array('id'));
+
+        // Conditionally launch create table for 'user_idps'.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Main savepoint reached
+        upgrade_main_savepoint(true, 2013092000.02);
+    }
+
     return true;
 }
